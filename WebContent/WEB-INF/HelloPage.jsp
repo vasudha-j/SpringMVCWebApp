ca13a03f4b47dfa54931a2d21112b294f92831cc
VASUDHA JAGARLAMUDY <jagarlamudy1991@gmail.com>
2018-03-02 15:49:17 -0600

    Tomcat and wildfly both ran fine

--- WebContent/WEB-INF/HelloPage.jsp
+++ WebContent/WEB-INF/HelloPage.jsp
@@ -1,6 +1,6 @@
 <html>
 <body>
- <h1> Spring MVC Application</h1>
- <h2> WELCOME </h2>
+	<h1>Spring MVC Application</h1>
+	<h2>WELCOME</h2>
 </body>
 </html>
--- src/com/vasudha/controller/HelloWorldController.java
+++ src/com/vasudha/controller/HelloWorldController.java
@@ -1,24 +1,30 @@
 package com.vasudha.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 
 /**
  * @author VJ
  *
  */
-public class HelloWorldController extends AbstractController {
-	
-	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) {
 
-		ModelAndView modelAndView = new ModelAndView("Hello Page");
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/welcome/country/{countryName}")
+	public ModelAndView helloWorld(@PathVariable("countryName") String countryName){
+		ModelAndView model = new ModelAndView("HelloPage");
+
+		model.addObject("msg", "hi"+countryName);
+		return model;
 		
-		modelAndView.addObject("welcomeMessage", "Hi! Welcome! ");
-	 
-		return modelAndView;
- }
+	}
 	
-
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) { 
+	        configurer.enable();
+	}
+	
 }
